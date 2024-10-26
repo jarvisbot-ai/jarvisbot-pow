3785ead672628c79f0f1336ad31305efa5ef55bb
HarryGuo2999 <harryguo2999@gmail.com>
2024-10-26 16:42:03 +0800

    initial commit

---
+++ setting.js
@@ -0,0 +1,5 @@
+const API_BASE_URL = '',
+    USE_LOCAL = true,
+    OSS_URL = USE_LOCAL ? '' : 'https://oss.jarvisbot.ai',
+    WHITE_PAPER_URL = 'https://oss.jarvisbot.ai/JarvisBot_Whitepaper.pdf',
+    VERSION = 'v1.0.8'
